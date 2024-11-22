248c7d50056ce7ec16d29da04c7ad3b71c9b60e4
Majo <usuario01@MacBook-Air-de-usuario01.local>
2024-11-22 15:22:46 -0500

    addddd

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyAPJpVaWLLIM4su6eWfzo5KD9taxDwWpz8",
+
+  authDomain: "desespero-55ca6.firebaseapp.com",
+
+  projectId: "desespero-55ca6",
+
+  storageBucket: "desespero-55ca6.firebasestorage.app",
+
+  messagingSenderId: "21928754986",
+
+  appId: "1:21928754986:web:1d4010b1a8568644706853",
+
+  measurementId: "G-STSQ8GX7P3"
+
+};
